e859f70dc1ff07afbd47217a709dcdcd5bcb5681
alan <ale@kea.dk>
2022-03-21 21:53:43 +0100

    sletter rum men ikke problemer

--- script/loadFromDb.js
+++ script/loadFromDb.js
@@ -97,7 +97,19 @@ function checkRoomExists() {
 }
 
 function sletRum() {
-  console.log(aktivtRum);
+  if (confirm("Vil du slette rummet?")) {
+    console.log(aktivtRum[0].id);
+    fetch(dbUrl + "/rooms" + "/" + aktivtRum[0].id, {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Authorization: apikey,
+        //  "cache-control": "no-cache"
+      },
+    })
+      .then((e) => e.json())
+      .then(alert("rum slettet"));
+  }
 }
 
 function start() {
